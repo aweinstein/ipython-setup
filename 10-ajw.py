2f7053403612f25ecdac17afe4e573a36eb85598
Alejandro Weinstein <alejandro.weinstein@gmail.com>
2017-08-22 14:58:02 -0300

    First commit.

---
+++ 10-ajw.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+get_ipython().magic(u'%matplotlib qt5')
+get_ipython().magic(u'%xmode plain')
+get_ipython().magic(u'%reload_ext autoreload')
+get_ipython().magic(u'%autoreload 2')

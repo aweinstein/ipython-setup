35079a0e30525c882048f2589615697c5483bf9e
Alejandro Weinstein <alejandro.weinstein@gmail.com>
2017-08-30 20:45:06 -0300

    Move magics to ipy file.

--- 10-ajw.py
+++ 10-ajw.py
@@ -1,7 +1,2 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
-get_ipython().magic(u'%matplotlib qt5')
-get_ipython().magic(u'%xmode plain')
-get_ipython().magic(u'%reload_ext autoreload')
-get_ipython().magic(u'%autoreload 2')
